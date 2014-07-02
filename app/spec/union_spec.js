309c2e155b55d247f46e3183be5f5e68a97f856c
srinivasg <srinu.gandikota@gmail.com>
2014-07-03 01:35:32 +0530

    performs set  union of arrays;

--- app/spec/union_spec.js
+++ app/spec/union_spec.js
@@ -1,5 +1,13 @@
 describe('union', function(){
+    it('should return [] if [], [] passed', function(){
+        expect(union([], [])).toEqual([]);
+    });
+
     it('should return union of two arrays', function(){
-        expect(union([1, 2, 3],[2, 3, 4])).toEqual([1, 2, 3, 4]);
+        expect(union([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    it('should return union of two arrays; should not include duplicates', function(){
+        expect(union([1, 2, 3, 4, 5, 6],[4, 5, 6, 7, 8, 9])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
     });
 });
--- app/src/union.js
+++ app/src/union.js
@@ -1,3 +1,14 @@
-var union = function(arr1, arr2){
-
+var union = function(){
+    var res = [];
+    function _union(arr){
+        each(arr, function(ele){
+            if(indexOf(res, ele) === -1){
+                res.push(ele)
+            }
+        });
+    }
+    each(arguments, function(arr){
+        _union(arr);
+    })
+   return res;
 };
