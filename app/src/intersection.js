3582f83dc6a997ee6dae10afab3e6b50c70218dc
srinivasg <srinu.gandikota@gmail.com>
2014-07-08 02:09:57 +0530

    refactored intersection function

--- app/src/intersection.js
+++ app/src/intersection.js
@@ -1,18 +1,10 @@
-var intersection = function(){
-    var res =_intersect(arguments[0], arguments[1]);
-    if(isEmpty(res))
-        return [];
-    each(rest(arguments, 2), function(arr){
-        res = _intersect(res, arr);
-    });
-
-    function _intersect(arr1, arr2){
+var intersection = function() {
+    return reduce(rest(arguments), first(arguments), function(res, arr){
         var common = [];
-        each(arr1, function(ele){
-            if(indexOf(arr2, ele) >=0)
+        each(res, function(ele) {
+            if(indexOf(arr, ele) >= 0)
                 common.push(ele);
         });
         return common;
-    }
-    return res;
+    });
 };
