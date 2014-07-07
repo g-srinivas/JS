8c1332d556a1527d514d28f2f905fc6080c9c18e
srinivasg <srinu.gandikota@gmail.com>
2014-07-08 02:09:28 +0530

    refactored union function

--- app/src/union.js
+++ app/src/union.js
@@ -1,14 +1,9 @@
-var union = function(){
-    var res = [];
-    function _union(arr){
-        each(arr, function(ele){
-            if(indexOf(res, ele) === -1){
+var union = function() {
+   return reduce(arguments, [], function(res, arr){
+        each(arr, function(ele) {
+            if(indexOf(res, ele) === -1)
                 res.push(ele)
-            }
         });
-    }
-    each(arguments, function(arr){
-        _union(arr);
-    })
-   return res;
+        return res;
+    });
 };
