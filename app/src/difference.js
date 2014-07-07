61d0cfb36bbeb8def99b6f185b00cf5eafdfc829
srinivasg <srinu.gandikota@gmail.com>
2014-07-08 02:10:14 +0530

    refactored difference function

--- app/src/difference.js
+++ app/src/difference.js
@@ -1,17 +1,10 @@
 var difference = function(){
-    var res = _diff(arguments[0], arguments[1]);
-    if(isEmpty(res))
-        return [];
-    each(rest(arguments, 2),function(arr){
-        res = _diff(res, arr);
-    });
-    return res;
-    function _diff(arr1, arr2){
-        var res = [];
-        each(arr1, function(ele){
-            if(indexOf(arr2, ele) === -1)
-                res.push(ele);
+    return reduce(rest(arguments), first(arguments), function(res, arr) {
+        var diff = [];
+        each(res, function(ele) {
+            if(indexOf(arr, ele) === -1)
+                diff.push(ele);
         });
-        return res;
-    }
+        return diff;
+    });
 };
